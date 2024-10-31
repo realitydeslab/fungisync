897dfde7dbf77d55ef189b44f395423447545793
agalloch21 <agalloch21@gmail.com>
2024-11-01 03:29:58 +0800

    sync2

--- Assets/Scripts/Network/NetcodeConnectionManager.cs
+++ Assets/Scripts/Network/NetcodeConnectionManager.cs
@@ -49,159 +49,67 @@ public class NetcodeConnectionManager : MonoBehaviour
     #region Event Listener
     void RegisterCallback()
     {
-        Debug.Log("Add All NetCode Listener");
+        Debug.Log($"[{this.GetType()}] Register NetCode Listener");
         NetworkManager.Singleton.OnClientConnectedCallback += OnClientConnectedCallback;
         NetworkManager.Singleton.OnClientDisconnectCallback += OnClientDisconnectCallback;
-        NetworkManager.Singleton.OnClientStarted += OnClientStarted;
-        NetworkManager.Singleton.OnClientStopped += OnClientStopped;
-        NetworkManager.Singleton.OnServerStarted += OnServerStarted;
-        NetworkManager.Singleton.OnServerStopped += OnServerStopped;
-        //networkManager.OnTransportFailure += OnTransportFailure;
-        //networkManager.OnConnectionEvent += OnConnectionEvent;
     }
 
     void UnregisterCallback()
     {
-        Debug.Log("Remove All NetCode Listener");
+        Debug.Log($"[{this.GetType()}] Unregister NetCode Listener");
         NetworkManager.Singleton.OnClientConnectedCallback -= OnClientConnectedCallback;
         NetworkManager.Singleton.OnClientDisconnectCallback -= OnClientDisconnectCallback;
-        NetworkManager.Singleton.OnClientStarted -= OnClientStarted;
-        NetworkManager.Singleton.OnClientStopped -= OnClientStopped;
-        NetworkManager.Singleton.OnServerStarted -= OnServerStarted;
-        NetworkManager.Singleton.OnServerStopped -= OnServerStopped;
-        //networkManager.OnTransportFailure -= OnTransportFailure;
-        //networkManager.OnConnectionEvent -= OnConnectionEvent;
     }
-
-    void OnClientStarted()
-    {
-        if(OnReceiveConnectionResultAction != null)
-        {
-            string msg = "Client successfully connected to server.";
-            OnReceiveConnectionResultAction?.Invoke(true, msg);
-            OnReceiveConnectionResultAction = null;
-        }
-
-        Debug.Log("Listener : OnClientStarted");
-
-        localIP = GetLocalIPAddress();
-    }
-
-    void OnClientStopped(bool result)
-    {
-        Debug.Log("Listener : OnClientStopped " + result);
-
-
-    }
-
-    void OnServerStarted()
-    {
-        Debug.Log("Listener : OnServerStarted");
-        localIP = GetLocalIPAddress();
-    }
-
-    void OnServerStopped(bool result)
-    {
-        Debug.Log("Listener : OnServerStopped " + result);
-    }
-
-    //void OnTransportFailure()
-    //{
-    //    Debug.Log("OnTransportFailure");
-    //}
-
-    //void OnConnectionEvent(NetworkManager manager, ConnectionEventData data)
-    //{
-    //    //Debug.Log("OnConnectionEvent | " + data.ClientId + "Remain Count " + manager.ConnectedClientsList.Count + "," + manager.ConnectedClients.Count + ", " + manager.ConnectedClientsIds.Count);
-    //}
 
     void OnClientConnectedCallback(ulong client_id)
     {
-        Debug.Log(string.Format("OnClientConnectedCallback | IsServer:{0}, IsClient:{1}, IsHost:{2}, ClientID:{3}", NetworkManager.Singleton.IsServer, NetworkManager.Singleton.IsClient, NetworkManager.Singleton.IsHost, client_id));
-
-        // If a new client joined
+        // As a server, if a new client joined
         if (NetworkManager.Singleton.IsServer)
         {
             OnClientJoinedEvent?.Invoke(client_id);
         }
 
-        if (NetworkManager.Singleton.IsClient)
+        // As a client, if successfully connected to a server
+        if (NetworkManager.Singleton.IsClient && NetworkManager.Singleton.IsHost == false)
         {
-            string msg = "Connected.";
+            string msg = "Client successfully connected to server.";
             OnReceiveConnectionResultAction?.Invoke(true, msg);
             OnReceiveConnectionResultAction = null;
+
+            localIP = GetLocalIPAddress();
         }
-
-
-        //// If it's in the process of establishing new connection
-        //if (OnReceiveConnectionResultAction != null)
-        //{
-        //    string msg = "";
-        //    if (NetworkManager.Singleton.IsHost)
-        //    {
-        //        msg = "Host established.";
-        //    }
-        //    else if (NetworkManager.Singleton.IsServer)
-        //    {
-        //        msg = "Server established.";
-        //    }
-        //    else if (NetworkManager.Singleton.IsClient)
-        //    {
-        //        msg = "Client established.";
-        //    }
-
-        //    OnReceiveConnectionResultAction?.Invoke(true, msg);
-        //    OnReceiveConnectionResultAction = null;
-        //}
-        //else
-        //{
-        //    // If a new client joined
-        //    if (NetworkManager.Singleton.IsServer)
-        //    {
-        //        OnClientJoinedEvent?.Invoke(client_id);
-        //    }
-        //}
     }
 
     void OnClientDisconnectCallback(ulong client_id)
     {
-        Debug.Log(string.Format("OnClientDisconnectCallback | IsServer:{0}, IsClient:{1}, ClientID:{2}", NetworkManager.Singleton.IsServer, NetworkManager.Singleton.IsClient, client_id));
 
-        // If it's in the process of establishing new connection
-        if (OnReceiveConnectionResultAction != null)
+        // As a server, when client left
+        if (NetworkManager.Singleton.IsServer)
         {
-            string msg = "";
-            if (NetworkManager.Singleton.IsHost)
-            {
-                msg = "Host establishment failed.";
-            }
-            else if (NetworkManager.Singleton.IsServer)
-            {
-                msg = "Server establishment failed.";
-            }
-            else if (NetworkManager.Singleton.IsClient)
-            {
-                msg = "Couldn't connect to server.";
-            }
-
-            OnReceiveConnectionResultAction?.Invoke(false, msg);
-            OnReceiveConnectionResultAction = null;
+            OnClientLostEvent?.Invoke(client_id);
         }
-        else
+
+        // As a client
+        if (NetworkManager.Singleton.IsClient && NetworkManager.Singleton.IsHost == false)
         {
-            // When client left
-            if (NetworkManager.Singleton.IsServer)
+            // When Client trys to connect with Server but failed
+            if (OnReceiveConnectionResultAction != null)
             {
-                OnClientLostEvent?.Invoke(client_id);
+                string msg = "Couldn't connect to server.";
+                OnReceiveConnectionResultAction?.Invoke(false, msg);
+                OnReceiveConnectionResultAction = null;
+
+                UnregisterCallback();
             }
 
-            // When client suddenly lost Server
-            if (NetworkManager.Singleton.IsClient)
+            // When suddenly lost Server
+            else
             {
                 OnServerLostEvent?.Invoke();
+
+                UnregisterCallback();
             }
         }
-
     }
         #endregion
 
@@ -209,91 +117,42 @@ public class NetcodeConnectionManager : MonoBehaviour
     {
         return System.Text.RegularExpressions.Regex.IsMatch(ip, @"^((25[0-5]|(2[0-4]|1\d|[1-9]|)\d)\.?\b){4}$");
     }
-    //public void SetServerIP(string ip)
-    //{
-    //    serverIP = ip;
-    //}
-
-    //void StartConnection(ConnectionMode mode, Action<bool, string> callback)
-    //{
-    //    if (mode == ConnectionMode.Server || mode == ConnectionMode.Host)
-    //    {
-    //        serverIP = "127.0.0.1";
-    //    }
-
-    //    if (IsIPAddressValide(serverIP) == false)
-    //    {
-    //        string msg = "Server ip is invalid.";
-
-    //        Debug.Log($"[{this.GetType()}] | {msg}");
-
-    //        callback?.Invoke(false, msg);
-
-    //        return;
-    //    }
-
-
-    //    SetConnectionData();
-
-    //    RegisterCallback();
-
-    //    Debug.Log($"[{this.GetType()}] | Starting {mode}.");
-
-    //    bool result = false;
-
-    //    switch(mode)
-    //    {
-    //        case ConnectionMode.Client:
-    //            NetworkManager.Singleton.StartClient();
-    //            break;
-    //        case ConnectionMode.Client:
-    //            NetworkManager.Singleton.StartClient();
-    //            break;
-    //        case ConnectionMode.Client:
-    //            NetworkManager.Singleton.StartClient();
-    //            break;
-    //    }
-            
-
-
-
-    //}
-
-    //void SetConnectionData()
-    //{
-    //    var unityTransport = NetworkManager.Singleton.GetComponent<UnityTransport>();
-    //    if (unityTransport != null)
-    //    {
-    //        unityTransport.SetConnectionData(serverIP, serverPort); // default is 7777
-    //    }
-    //}
+    
 
     #region Public Functions
     public void StartClient(Action<bool, string> callback)
     {
+        if (NetworkManager.Singleton == null)
+        {
+            Debug.Log($"[{this.GetType()}] Please wait for NetworkManager to initialize");
+
+            return;
+        }
+
+        if (NetworkManager.Singleton.IsConnectedClient || NetworkManager.Singleton.IsListening)
+        {
+            Debug.Log($"[{this.GetType()}] Connection has already been established. Please shutdown first.");
+
+            return;
+        }
+
         if (IsIPAddressValide(serverIP) == false)
         {
             string msg = "Server ip is invalid.";
 
-            Debug.Log($"[{this.GetType()}] | {msg}");
+            Debug.Log($"[{this.GetType()}] {msg}");
 
             callback?.Invoke(false, msg);
 
             return;
         }
-
-        if(NetworkManager.Singleton == null)
-        {
-            Debug.Log($"[{this.GetType()}] | Please wait for NetworkManager to initialize");
-
-            return;
-        }
+        
 
         OnBeforeClientStarted();
 
         RegisterCallback();
 
-        Debug.Log($"[{this.GetType()}] | Starting Client.");
+        Debug.Log($"[{this.GetType()}] Starting Client.");
 
         bool result = NetworkManager.Singleton.StartClient();
 
@@ -305,9 +164,11 @@ public class NetcodeConnectionManager : MonoBehaviour
         {
             string msg = "Failed to start client.";
 
-            Debug.Log($"[{this.GetType()}] | {msg}");
+            Debug.Log($"[{this.GetType()}] {msg}");
 
             callback?.Invoke(false, msg);
+
+            UnregisterCallback();
         }
     }
 
@@ -315,7 +176,14 @@ public class NetcodeConnectionManager : MonoBehaviour
     {
         if (NetworkManager.Singleton == null)
         {
-            Debug.Log($"[{this.GetType()}] | Please wait for NetworkManager to initialize");
+            Debug.Log($"[{this.GetType()}] Please wait for NetworkManager to initialize");
+
+            return;
+        }
+
+        if (NetworkManager.Singleton.IsConnectedClient || NetworkManager.Singleton.IsListening)
+        {
+            Debug.Log($"[{this.GetType()}] Connection has already been established. Please shutdown first.");
 
             return;
         }
@@ -324,7 +192,7 @@ public class NetcodeConnectionManager : MonoBehaviour
 
         RegisterCallback();
 
-        Debug.Log($"[{this.GetType()}] | Starting Server.");
+        Debug.Log($"[{this.GetType()}] Starting Server.");
 
         // Will send back result instantly
         bool result = NetworkManager.Singleton.StartServer();
@@ -334,13 +202,24 @@ public class NetcodeConnectionManager : MonoBehaviour
         Debug.Log($"[{this.GetType()}] {msg}");
 
         callback?.Invoke(result, msg);
+
+        localIP = GetLocalIPAddress();
+
+        if (result == false) UnregisterCallback();
     }
 
     public void StartHost(Action<bool, string> callback)
     {
         if (NetworkManager.Singleton == null)
         {
-            Debug.Log($"[{this.GetType()}] | Please wait for NetworkManager to initialize");
+            Debug.Log($"[{this.GetType()}] Please wait for NetworkManager to initialize");
+
+            return;
+        }
+
+        if (NetworkManager.Singleton.IsConnectedClient || NetworkManager.Singleton.IsListening)
+        {
+            Debug.Log($"[{this.GetType()}] Connection has already been established. Please shutdown first.");
 
             return;
         }
@@ -349,7 +228,7 @@ public class NetcodeConnectionManager : MonoBehaviour
 
         RegisterCallback();
 
-        Debug.Log($"[{this.GetType()}] | Starting Host.");
+        Debug.Log($"[{this.GetType()}] Starting Host.");
 
         // Will send back result instantly
         bool result = NetworkManager.Singleton.StartHost();
@@ -359,25 +238,30 @@ public class NetcodeConnectionManager : MonoBehaviour
         Debug.Log($"[{this.GetType()}] {msg}");
 
         callback?.Invoke(result, msg);
+
+        localIP = GetLocalIPAddress();
+
+        if (result == false) UnregisterCallback();
     }
 
     public void ShutDown()
     {
         if (NetworkManager.Singleton == null)
         {
-            Debug.Log($"[{this.GetType()}] | Please wait for NetworkManager to initialize");
+            Debug.Log($"[{this.GetType()}] Please wait for NetworkManager to initialize");
 
             return;
         }
 
-        if (NetworkManager.Singleton.IsClient || NetworkManager.Singleton.IsServer || NetworkManager.Singleton.IsHost)
+        if(NetworkManager.Singleton.IsConnectedClient || NetworkManager.Singleton.IsListening)
         {
-            Debug.Log($"[{this.GetType()}] | Shuting down.");
+            Debug.Log($"[{this.GetType()}] Shuting down.");
 
             NetworkManager.Singleton.Shutdown();
+
+            UnregisterCallback();
         }
-            
-    }
+    } 
     #endregion
 
     void OnBeforeHostStarted()
