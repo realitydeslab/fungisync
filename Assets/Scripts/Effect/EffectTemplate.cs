822b6fd93bdb8ba5dbaad4c1ff82f9ead15827be
agalloch21 <agalloch21@gmail.com>
2024-11-09 23:26:27 +0800

    organize effect folder

---
+++ Assets/Scripts/Effect/EffectTemplate.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class EffectTemplate : EffectBase
+{
+
+
+    public override void StartEffect()
+    {
+        base.StartEffect();
+    }
+
+    public override void StopEffect()
+    {
+        base.StopEffect();
+    }
+
+    public override void UpdateVFXParameter()
+    {
+        base.UpdateVFXParameter();
+
+        // set custom parameter
+        // 
+    }
+}
