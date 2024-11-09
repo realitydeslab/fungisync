f2ea7ac7fa3004eeb1504baee9e2729f034779b7
agalloch21 <agalloch21@gmail.com>
2024-11-10 01:26:38 +0800

    add new effect

---
+++ Assets/Scripts/Effect/Effect4_Lantern.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class Effect4_Lantern : EffectBase
+{
+    public override void StartEffect()
+    {
+        base.StartEffect();
+
+    }
+
+    public override void StopEffect()
+    {
+        base.StopEffect();
+    }
+
+    public override void UpdateVFXParameter()
+    {
+        base.UpdateVFXParameter();
+
+        // set custom parameter
+        // 
+    }
+}
